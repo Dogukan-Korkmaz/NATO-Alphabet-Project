8987b5d14ad8b38cf23d288671d1ea9753f23e0c
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-02-12 20:03:20 +0300

    try-except added.

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word ").upper()
 
-letters = [new_nato_dict[letter] for letter in word]
-print(letters)
+# is_finished = False
+# while not is_finished:
+#     word = input("Enter a word ").upper()
+#     try:
+#         letters = [new_nato_dict[letter] for letter in word]
+#     except:
+#         print("Sorry, only letters in the alphabet please.😇")
+#     else:
+#         is_finished = True
+#         print(letters)
 
+def generate_phonetic():
+    word = input("Enter a word ").upper()
+    try:
+        letters = [new_nato_dict[letter] for letter in word]
+    except:
+        print("Sorry, only letters in the alphabet please.😇")
+        generate_phonetic()
+    else:
+        is_finished = True
+        print(letters)
 
+generate_phonetic()
